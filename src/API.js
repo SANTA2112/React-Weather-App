9850a8d06244df5f32bc6f5db6219373c4d1956b
Santa <s.yushmanov777@gmail.com>
2018-09-15 20:04:42 +0700

    Create function for take user coords

---
+++ src/API.js
@@ -0,0 +1,10 @@
+const API_URL = 'http://api.openweathermap.org/data/2.5/weather';
+
+const API_KEY = '&APPID=8b5002fb81de9e5f220897fe5cbf9b38';
+
+const getJSON = (...args) => fetch(...args).then(r => r.json());
+
+export const getWeather = (lat, log) => getJSON(`${API_URL}?lat=${lat}&lon=${log}${API_KEY}`).then(console.log);
+
+//;
+
--- src/Weather/Weather.jsx
+++ src/Weather/Weather.jsx
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
 import MainBlock from '../MainBlock/';
+import { getWeather, kek } from "../API";
 import { Wrapper } from "./styled";
 import default_weather_img from '../_assets/images/default_weather_img.jpg';
 
 class Weather extends Component {
   state = {
     image: default_weather_img,
+    city: '',
   }
+
+  componentDidMount() {
+    this.getUserCoords();
+  }
+
+  getUserCoords() {
+    window.navigator.geolocation.getCurrentPosition(({ coords }) => {
+      getWeather(coords.latitude, coords.longitude);
+    });
+  }
+
   render() {
     const { image } = this.state;
+
     return (
       <Wrapper>
         <MainBlock image={image}/>
